53c85e96fea852e5afee6e090be1847c56f6f6ae
AbYahaya <yabdulrauf911@gmail.com>
2024-10-04 17:38:05 +0000

    adv 2

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employeeNames = [];
+
+  // Iterate through the reportWithIterator and collect employee names
+  for (const employee of reportWithIterator) {
+    employeeNames.push(employee);
+  }
+
+  // Join the names with " | " and return the result
+  return employeeNames.join(' | ');
+}
