bef481df6a8821f8bcee1348cdeaced18a14d857
AbYahaya <yabdulrauf911@gmail.com>
2024-10-04 19:55:20 +0000

    task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(); // Create a new instance of the class using its constructor
+  }
+}
