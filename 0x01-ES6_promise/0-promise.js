2b813a920ad7690e587e4776db7eb92ec38f7091
AbYahaya <yabdulrauf911@gmail.com>
2024-10-06 00:50:49 +0000

    task 1

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can leave the promise logic empty for now, or add any condition
+    resolve();
+  });
+}
